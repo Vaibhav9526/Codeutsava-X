d8de00c089ecb302140ae80c59583113b230d965
mayankj0919 <mayankjoshi0919@gmail.com>
2026-08-22 22:47:42 +0530

    Simplify navbar navigation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,12 +9,6 @@ export function Navbar() {
         <a href='#faq'>FAQ</a>
         <a href='#contact'>CONTACT</a>
       </nav>
-
-      <a className={styles.navCta} href='https://discord.gg/Ek9gr2Xnqb'>
-        <span className={styles.liveDot} aria-hidden='true' />
-        JOIN THE COMMUNITY
-        <span aria-hidden='true'>-&gt;</span>
-      </a>
     </header>
   );
 }
