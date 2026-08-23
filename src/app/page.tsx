cdac331b2a010eb95900a1fcad6f6e1e138742db
mayankj0919 <mayankjoshi0919@gmail.com>
2026-08-23 16:50:18 +0530

    feat: add themed about section and encrypt register CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { GlitchverseHero } from '@/components/hero/GlitchverseHero';
 import { SponsorSection } from "@/components/sponsor-section/SponsorSection";
 import { TimelineRoad } from '@/components/timeline/TimelineRoad';
 import { EventSections } from '@/components/sections/EventSections';
+import { AboutSection } from '@/components/sections/about-section';
 import { BackgroundVideo } from '@/components/layout/BackgroundVideo';
 
 export const metadata: Metadata = {
@@ -13,17 +14,17 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-      <>
-    <BackgroundVideo />
+    <>
+      <BackgroundVideo />
       <ExperienceShell>
         <div className='flex min-h-screen flex-col bg-transparent'>
           <GlitchverseHero />
-        <SponsorSection />
-        <TimelineRoad />
-        <EventSections />
-      </div>
-    </ExperienceShell>
-      </>
-
+          <AboutSection />
+          <SponsorSection />
+          <TimelineRoad />
+          <EventSections />
+        </div>
+      </ExperienceShell>
+    </>
   );
 }
